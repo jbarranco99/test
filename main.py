90766c0e8117ea8e8e852ade2814cbc3210d4174
jbarranco99 <161100490+jbarranco99@users.noreply.github.com>
2024-02-24 19:05:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
+    req_data = request.get_json()
     pickedCats = req_data['pickedCats']
     return jsonify("pickedCats":pickedCats)
 
